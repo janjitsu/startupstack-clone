2c5d6de119727d41c8b3b934c9589db35900a888
janfrs <janfrs3@gmail.com>
2016-02-15 18:03:39 -0200

    Remove ActiveRecord References
    after removing sqlite3 from application. It complaints about sqlite3 adapter on
    database.yml because ActiveRecord references it, we don't need it for mongoid

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'https://rubygems.org'
 ruby "2.3.0"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
-gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  #config.active_record.dump_schema_after_migration = false
 
   #landing jobs api url
   config.api_base_url = 'https://landing.jobs/api/v1/'
